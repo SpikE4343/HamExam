d1e4e5659b995cf0b2ef991543fee1adb07c868d
John Rohrssen <johnrohrssen@gmail.com>
2016-08-27 00:44:12 -0700

    Log port

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const app = require('./app');
 const port = process.env.PORT || app.get('port');
 const server = app.listen(port);
 
+console.log( `port = ${process.env.PORT}`);
 server.on('listening', () =>
   console.log(`Feathers application started on ${app.get('host')}:${port}`)
 );
